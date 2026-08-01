e3b831042524e9f15fed9f0f3253f203ba7fc130
Nikita Bhansali <208110937+nikitaaaa123@users.noreply.github.com>
2026-08-01 15:30:11 +0530

    Time: 1 ms (51.87%), Space: 42.8 MB (57.54%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,24 @@
+class Solution {
+    private Integer[][] dp;
+
+    private int solve(int[] nums, int i, int j) {
+        if (i == j) {
+            return nums[i];
+        }
+
+        if (dp[i][j] != null) {
+            return dp[i][j];
+        }
+
+        int takeLeft = nums[i] - solve(nums, i + 1, j);
+        int takeRight = nums[j] - solve(nums, i, j - 1);
+
+        return dp[i][j] = Math.max(takeLeft, takeRight);
+    }
+
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        dp = new Integer[n][n];
+        return solve(nums, 0, n - 1) >= 0;
+    }
+}
