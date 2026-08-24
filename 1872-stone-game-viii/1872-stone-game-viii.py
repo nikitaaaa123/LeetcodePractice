5d357805968cfd3a17d6884f3aca098b2c1402c1
Nikita Bhansali <208110937+nikitaaaa123@users.noreply.github.com>
2026-08-24 10:01:29 +0530

    Time: 659 ms (80.47%), Space: 33 MB (75.15%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def stoneGameVIII(self, stones: List[int]) -> int:
+        n = len(stones)
+
+        # Prefix sums
+        for i in range(1, n):
+            stones[i] += stones[i - 1]
+
+        best = stones[-1]
+
+        for i in range(n - 2, 0, -1):
+            best = max(best, stones[i] - best)
+
+        return best
